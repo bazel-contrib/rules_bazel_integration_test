982569c21533f60daf61f03525e14fad553010d2
Chuck Grindel <chuck@chuckgrindel.com>
2021-11-12 16:55:34 -0700

    Moved common stuff to its own lib.

---
+++ tools/common.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+exit_on_error() {
+  local err_msg="${1:-}"
+  [[ -n "${err_msg}" ]] || err_msg="Unspecified error occurred."
+  echo >&2 "${err_msg}"
+  exit 1
+}
+
+normalize_path() {
+  local path="${1}"
+  if [[ -d "${path}" ]]; then
+    local dirname="${path}"
+  else
+    local dirname="$(dirname "${path}")"
+    local basename="$(basename "${path}")"
+  fi
+  dirname="$(cd "${dirname}" > /dev/null && pwd)"
+  if [[ -z "${basename:-}" ]]; then
+    echo "${dirname}"
+  else
+    echo "${dirname}/${basename}"
+  fi
+}
+
+# Lovingly inspired by https://unix.stackexchange.com/a/13474.
+upsearch() {
+  local target_file="${1}"
+  slashes=${PWD//[^\/]/}
+  directory="$PWD"
+  for (( n=${#slashes}; n>0; --n ))
+  do
+    local test_path="${directory}/${target_file}"
+    test -e "${test_path}" && echo "${test_path}" && return 
+    directory="${directory}/.."
+  done
+}
+
+sort_items() {
+  local IFS=$'\n'
+  shift
+  sort -u <<<"$*"
+}
+
+print_by_line() {
+  for item in "${@}" ; do
+    echo "${item}"
+  done
+}
+
--- tools/find_child_workspace_packages.sh
+++ tools/find_child_workspace_packages.sh
@@ -6,66 +6,42 @@
 # For integration tests, we want to be able to glob() up the sources inside a nested package
 # See explanation in .bazelrc
 
-set -euo pipefail
+# set -euo pipefail
+
+# --- begin runfiles.bash initialization v2 ---
+# Copy-pasted from the Bazel Bash runfiles library v2.
+set -uo pipefail; f=bazel_tools/tools/bash/runfiles/runfiles.bash
+source "${RUNFILES_DIR:-/dev/null}/$f" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "${RUNFILES_MANIFEST_FILE:-/dev/null}" | cut -f2- -d' ')" 2>/dev/null || \
+  source "$0.runfiles/$f" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "$0.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "$0.exe.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
+  # { echo>&2 "ERROR: cannot find $f"; exit 1; }; f=; set -e
+  f=; set -e
+# --- end runfiles.bash initialization v2 ---
+
+# Do not use helper functions as they have not been loaded yet
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null && pwd)"
+
+# If we are being run via `bazel run` the rlocation function will exist and we
+# will load the common functions using rlocation. Otherwise, we are being executed
+# directly.
+if [[ $(type -t rlocation) == function ]]; then
+  common_lib="$(rlocation cgrindel_rules_bazel_integration_test/tools/common.sh)"
+else
+  common_lib="${script_dir}/common.sh"
+fi
+source "${common_lib}"
 
 # MARK - Functions
-
-exit_on_error() {
-  local err_msg="${1:-}"
-  [[ -n "${err_msg}" ]] || err_msg="Unspecified error occurred."
-  echo >&2 "${err_msg}"
-  exit 1
-}
-
-normalize_path() {
-  local path="${1}"
-  if [[ -d "${path}" ]]; then
-    local dirname="${path}"
-  else
-    local dirname="$(dirname "${path}")"
-    local basename="$(basename "${path}")"
-  fi
-  dirname="$(cd "${dirname}" > /dev/null && pwd)"
-  if [[ -z "${basename:-}" ]]; then
-    echo "${dirname}"
-  else
-    echo "${dirname}/${basename}"
-  fi
-}
-
-# Lovingly inspired by https://unix.stackexchange.com/a/13474.
-upsearch() {
-  local target_file="${1}"
-  slashes=${PWD//[^\/]/}
-  directory="$PWD"
-  for (( n=${#slashes}; n>0; --n ))
-  do
-    local test_path="${directory}/${target_file}"
-    test -e "${test_path}" && echo "${test_path}" && return 
-    directory="${directory}/.."
-  done
-}
 
 find_bazel_pkgs() {
   local path="${1}"
   find "${path}" \( -name BUILD -or -name BUILD.bazel \) | xargs -n 1 dirname 
 }
 
-sort_items() {
-  local IFS=$'\n'
-  shift
-  sort -u <<<"$*"
-}
-
-print_by_line() {
-  for item in "${@}" ; do
-    echo "${item}"
-  done
-}
-
 # MARK - Main
 
-script_dir="$(normalize_path "${BASH_SOURCE[0]}")"
 starting_dir=$(pwd)
 
 # Make sure that we end up back in the original directory.
