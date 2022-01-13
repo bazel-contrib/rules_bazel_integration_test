56a12bea888868858e57694afbb25c014c0a1103
Chuck Grindel <chuck@chuckgrindel.com>
2022-01-13 13:32:00 -0700

    Added test with dynamic test runner.

---
+++ examples/dynamic_workspace_test_runner.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+# --- begin runfiles.bash initialization v2 ---
+# Copy-pasted from the Bazel Bash runfiles library v2.
+set -uo pipefail; f=bazel_tools/tools/bash/runfiles/runfiles.bash
+source "${RUNFILES_DIR:-/dev/null}/$f" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "${RUNFILES_MANIFEST_FILE:-/dev/null}" | cut -f2- -d' ')" 2>/dev/null || \
+  source "$0.runfiles/$f" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "$0.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "$0.exe.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
+  { echo>&2 "ERROR: cannot find $f"; exit 1; }; f=; set -e
+# --- end runfiles.bash initialization v2 ---
+
+
+# MARK - Locate Deps
+
+assertions_sh_location=cgrindel_bazel_starlib/shlib/lib/assertions.sh
+assertions_sh="$(rlocation "${assertions_sh_location}")" || \
+  (echo >&2 "Failed to locate ${assertions_sh_location}" && exit 1)
+source "${assertions_sh}"
+
+
+# MARK - Process Args
+
+# Process args
+while (("$#")); do
+  case "${1}" in
+    "--bazel")
+      bazel="${2}"
+      shift 2
+      ;;
+    "--workspace")
+      workspace_dir="${2}"
+      shift 2
+      ;;
+    *)
+      fail "Unrecognized argument. ${@}"
+      ;;
+  esac
+done
+
+[[ -n "${bazel:-}" ]] || exit_with_msg "Must specify the location of the Bazel binary."
+[[ -n "${workspace_dir:-}" ]] || exit_with_msg "Must specify the path of the workspace directory."
+
+
+# MARK - Create a WORKSPACE
+
+cd "${workspace_dir}"
+
+# Create a WORKSPACE file
+touch WORKSPACE
+
+# Run Bazel info
+info_out="$( "${bazel}" info release )"
+assert_match "release " "${info_out}" "Expected a release stanza."
