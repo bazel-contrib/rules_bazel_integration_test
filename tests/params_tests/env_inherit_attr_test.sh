4245e7baafc9a024e4edd4a6e205a1ba7bffb0b4
Chuck Grindel <chuck.grindel@robinhood.com>
2022-03-15 10:29:26 -0600

    Added placeholder for attr test.

---
+++ tests/params_tests/env_inherit_attr_test.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+# --- begin runfiles.bash initialization v2 ---
+# Copy-pasted from the Bazel Bash runfiles library v2.
+set -uo pipefail; f=bazel_tools/tools/bash/runfiles/runfiles.bash
+source "${RUNFILES_DIR:-/dev/null}/$f" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "${RUNFILES_MANIFEST_FILE:-/dev/null}" | cut -f2- -d' ')" 2>/dev/null || \
+  source "$0.runfiles/$f" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "$0.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "$0.exe.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
+  { echo>&2 "ERROR: cannot find $f"; exit 1; }; f=; set -e
+# --- end runfiles.bash initialization v2 ---
+
+# MARK - Locate Deps
+
+assertions_sh_location=cgrindel_bazel_starlib/shlib/lib/assertions.sh
+assertions_sh="$(rlocation "${assertions_sh_location}")" || \
+  (echo >&2 "Failed to locate ${assertions_sh_location}" && exit 1)
+source "${assertions_sh}"
+
+
+# MARK - Process Args
+
+query_output="${1}"
+shift 1
+
+expected_env_inherit_values=()
+while (("$#")); do
+  expected_env_inherit_values+=( "${1}" )
+  shift 1
+done
+
+[[ ${#expected_env_inherit_values} == 0 ]] && fail "No expected values were provided."
+
+
+# MARK - Test
+
+
+
+fail "IMPLEMENT ME!"
