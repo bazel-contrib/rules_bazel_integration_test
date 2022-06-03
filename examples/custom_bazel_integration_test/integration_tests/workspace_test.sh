7e6826a54586ecb9ab9e08ab8861113527c8072e
k1nkreet <polyakovskiy.ilya@gmail.com>
2022-06-03 14:42:32 +0300

    Clenaup in test runners

--- examples/custom_bazel_integration_test/integration_tests/workspace_test.sh
+++ examples/custom_bazel_integration_test/integration_tests/workspace_test.sh
@@ -1,6 +1,4 @@
 set -uo pipefail
 
 output=$($BIT_BAZEL_BINARY --output_user_root=$BIT_OUTPUT_USER_ROOT run //:foo_test)
-test "$output" == "Foo Test" || {
-    >&2 echo "output is wrong: $output" && exit 1;
-}
+test "$output" == "Foo Test" || ( echo >&2 "Output is invalid: $output" && exit 1 )
--- examples/custom_bazel_integration_test/test_runner_wrapper.sh
+++ examples/custom_bazel_integration_test/test_runner_wrapper.sh
@@ -7,8 +7,7 @@ source "${RUNFILES_DIR:-/dev/null}/$f" 2>/dev/null || \
     source "$(grep -sm1 "^$f " "$0.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
     source "$(grep -sm1 "^$f " "$0.exe.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
     { echo>&2 "ERROR: cannot find $f"; exit 1; }; f=; set -e
-        # --- end runfiles.bash initialization v2 ---
-        #
+# --- end runfiles.bash initialization v2 ---
 
 create_scratch_dir_sh_location=contrib_rules_bazel_integration_test/tools/create_scratch_dir.sh
 create_scratch_dir_sh="$(rlocation "${create_scratch_dir_sh_location}")" || \
