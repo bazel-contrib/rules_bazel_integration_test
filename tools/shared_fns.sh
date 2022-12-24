ef519afd270781a41a54b485e5b54462790e2eb5
John Firebaugh <john.firebaugh@gmail.com>
2022-12-23 20:06:00 -0800

    Add support for WORKSPACE.bazel Fixes #97

--- tools/shared_fns.sh
+++ tools/shared_fns.sh
@@ -4,5 +4,5 @@ find_workspace_dirs() {
   local path="${1}"
   # Make sure that the -print0 is the last primary for find. Otherwise, you
   # will get undesirable results.
-  find "${path}" -name "WORKSPACE" -print0  | xargs -0 -n 1 dirname
+  find "${path}" -name "WORKSPACE" -o -name "WORKSPACE.bazel" -print0  | xargs -0 -n 1 dirname
 }
